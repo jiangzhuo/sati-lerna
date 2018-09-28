9a85e161b64a905d24c17d947ab02551f91adc31
jiangzhuo <glmmmmmm@163.com>
2018-09-29 02:16:59 +0800

    上传背景上传audio

--- packages/sati/src/main.ts
+++ packages/sati/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     const logger = new Logger('Notadd');
     logger.log(APP_CONFIG.banner);
 
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, { cors: true });
 
     await app.listen(5000, '0.0.0.0', () => {
         logger.log('Notadd GraphQL IDE Server started on: http://localhost:5000/graphql');
--- packages/sati/src/modules/upload/upload.controller.ts
+++ packages/sati/src/modules/upload/upload.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Inject, Post, Get, UseInterceptors, FileInterceptor, UploadedFile } from '@nestjs/common';
-import * as UUID from 'uuid/v1';
+import { Controller, Inject, Post, Get, Options, UseInterceptors, FileInterceptor, UploadedFile } from '@nestjs/common';
+// import * as UUID from 'uuid/v1';
 import * as OSS from 'ali-oss';
+import * as hasha from 'hasha';
+import * as mime from 'mime';
+
 import { ACCESS_KEY_ID, ACCESS_KEY_SECRET, REGION, AVATAR_BUCKET } from '../../configurations/oss.config';
 
 @Controller()
@@ -16,7 +19,39 @@ export class UploadController {
             accessKeySecret: ACCESS_KEY_SECRET,
             bucket: AVATAR_BUCKET
         });
-        const result = await client.put(`avatar/${UUID()}.jpg`, file.buffer);
+        // let hash = hasha(file.buffer, { algorithm: 'md5' });
+        // console.log(hash);
+        const result = await client.put(`avatar/${hasha(file.buffer, { algorithm: 'md5' })}.${mime.getExtension(file.mimetype)}`, file.buffer);
         return { code: 200, message: 'upload avatar success', data: result.res.requestUrls[0] };
+    }
+
+    @Post('uploadBackground')
+    @UseInterceptors(FileInterceptor('file'))
+    async uploadBackground(@UploadedFile() file) {
+        const client = new OSS({
+            region: REGION,
+            accessKeyId: ACCESS_KEY_ID,
+            accessKeySecret: ACCESS_KEY_SECRET,
+            bucket: AVATAR_BUCKET
+        });
+        // let hash = hasha(file.buffer, { algorithm: 'md5' });
+        // console.log(hash);
+        const result = await client.put(`background/${hasha(file.buffer, { algorithm: 'md5' })}.${mime.getExtension(file.mimetype)}`, file.buffer);
+        return { code: 200, message: 'upload background success', data: result.res.requestUrls[0] };
+    }
+
+    @Post('uploadAudio')
+    @UseInterceptors(FileInterceptor('file'))
+    async uploadAudio(@UploadedFile() file) {
+        const client = new OSS({
+            region: REGION,
+            accessKeyId: ACCESS_KEY_ID,
+            accessKeySecret: ACCESS_KEY_SECRET,
+            bucket: AVATAR_BUCKET
+        });
+        // let hash = hasha(file.buffer, { algorithm: 'md5' });
+        // console.log(hash);
+        const result = await client.put(`audio/${hasha(file.buffer, { algorithm: 'md5' })}.${mime.getExtension(file.mimetype)}`, file.buffer);
+        return { code: 200, message: 'upload audio success', data: result.res.requestUrls[0] };
     }
 }
