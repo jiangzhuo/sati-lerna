05a3f4470bb15ccd655c72361a713ed12720390c
jiangzhuo <glmmmmmm@163.com>
2018-10-22 23:23:47 +0800

    初始化和定时更新resource缓存

--- packages/sati/src/modules/resource/cache/resource.cache.ts
+++ packages/sati/src/modules/resource/cache/resource.cache.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Optional } from '@nestjs/common';
 import { isArray } from 'lodash';
+import { NotaddGrpcClientFactory } from '../../../grpc/grpc.client-factory';
 // import { validateEach } from "@nestjs/common/utils/validate-each.util";
 // import * as SortedMap from 'collections/sorted-map';
 // import * as FastMap from 'collections/fast-map';
@@ -8,8 +9,30 @@ import { isArray } from 'lodash';
 
 @Injectable()
 export class ResourceCache {
+    async onModuleInit(): Promise<any> {
+        this.mindfulnessServiceInterface = this.notaddGrpcClientFactory.resourceModuleClient.getService('MindfulnessService');
+        this.natureServiceInterface = this.notaddGrpcClientFactory.resourceModuleClient.getService('NatureService');
+        this.wanderServiceInterface = this.notaddGrpcClientFactory.resourceModuleClient.getService('WanderService');
+        await this.initResourceCache();
+        setInterval(() => {
+            this.initResourceCache().then(() => {
+
+            }).catch((e) => {
+                console.log(e);
+            });
+        }, 60000);
+    }
+
+    constructor(
+        @Inject(NotaddGrpcClientFactory) private readonly notaddGrpcClientFactory: NotaddGrpcClientFactory
+    ) {
+    }
+
     private cache = new Map();
     private cacheSortedByCreateTime = [];
+    private mindfulnessServiceInterface;
+    private natureServiceInterface;
+    private wanderServiceInterface;
 
     public updateResourceCache(resources, type: string) {
         if (!isArray(resources)) {
@@ -29,11 +52,58 @@ export class ResourceCache {
         if (startIndex === -1) {
             startIndex = 0;
         }
-        const resourceIds = this.cacheSortedByCreateTime.slice(startIndex, startIndex + limit);
+        const resourceIds = this.cacheSortedByCreateTime.slice(startIndex + 1, startIndex + 1 + limit);
         return resourceIds.map(resourceId => this.cache.get(resourceId));
     }
 
     private async initResourceCache() {
         // 初始化缓存
+        const batchGetLimit = 1;
+        try {
+
+            // mindfulness
+            let mindfulnessResult = await this.mindfulnessServiceInterface.getMindfulness({ first: batchGetLimit }).toPromise();
+            while (mindfulnessResult.data.length &&
+            mindfulnessResult.data.length !== 0 &&
+            mindfulnessResult.data.some((mindfulness) => !this.cache.has(mindfulness.id))) {
+                this.updateResourceCache(mindfulnessResult.data, 'mindfulness');
+                mindfulnessResult = await this.mindfulnessServiceInterface.getMindfulness(
+                    { first: batchGetLimit, after: mindfulnessResult.data[mindfulnessResult.data.length - 1].id }
+                ).toPromise();
+            }
+            // nature
+            let natureResult = await this.natureServiceInterface.getNature({ first: batchGetLimit }).toPromise();
+            while (natureResult.data.length &&
+            natureResult.data.length !== 0 &&
+            natureResult.data.some((nature) => !this.cache.has(nature.id))) {
+                this.updateResourceCache(natureResult.data, 'nature');
+                natureResult = await this.natureServiceInterface.getNature(
+                    { first: batchGetLimit, after: natureResult.data[natureResult.data.length - 1].id }
+                ).toPromise();
+            }
+            // wander
+            let wanderResult = await this.wanderServiceInterface.getWander({ first: batchGetLimit }).toPromise();
+            while (wanderResult.data.length &&
+            wanderResult.data.length !== 0 &&
+            wanderResult.data.some((wander) => !this.cache.has(wander.id))) {
+                this.updateResourceCache(wanderResult.data, 'wander');
+                wanderResult = await this.wanderServiceInterface.getWander(
+                    { first: batchGetLimit, after: wanderResult.data[wanderResult.data.length - 1].id }
+                ).toPromise();
+            }
+            // wanderAlbum
+            let wanderAlbumResult = await this.wanderServiceInterface.getWanderAlbum({ first: batchGetLimit }).toPromise();
+            while (wanderAlbumResult.data.length &&
+            wanderAlbumResult.data.length !== 0 &&
+            wanderAlbumResult.data.some((wanderAlbum) => !this.cache.has(wanderAlbum.id))) {
+                this.updateResourceCache(wanderAlbumResult.data, 'wanderAlbum');
+                wanderAlbumResult = await this.wanderServiceInterface.getWanderAlbum(
+                    { first: batchGetLimit, after: wanderAlbumResult.data[wanderAlbumResult.data.length - 1].id }
+                ).toPromise();
+            }
+        } catch (e) {
+            console.error('init resource cache failed!');
+            console.error(e);
+        }
     }
 }
